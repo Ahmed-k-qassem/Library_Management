43d9c332242ef8bd2c7857155f986d6222539eda
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-04 16:52:25 +0300

    used List.of instead of arrays.aslist approach then deleted unused library of Arrays

--- src/main/java/com/librarymanagment/LibraryManagment/Security/UserSecurity.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Security/UserSecurity.java
@@ -7,20 +7,20 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Arrays;
+
 import java.util.Collection;
 import java.util.List;
 
 
 public class UserSecurity implements UserDetails {
-     private User user;
+     private final User user;
      public UserSecurity(User user){
          this.user = user;
      }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
     }
 
     @Override
