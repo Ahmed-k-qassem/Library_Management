f22dbf4e3ac0a11e3912e54de430d449e5b7916c
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-15 16:58:32 +0300

    injected the authentication provider in the filter chain and added a permit for the post methods

--- src/main/java/com/librarymanagment/LibraryManagment/config/SecurityConfig.java
+++ src/main/java/com/librarymanagment/LibraryManagment/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.librarymanagment.LibraryManagment.config;
 
+import com.librarymanagment.LibraryManagment.Security.CustomAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,22 +12,30 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final CustomAuthenticationProvider customAuthenticationProvider;
+
+    public SecurityConfig(CustomAuthenticationProvider customAuthenticationProvider) {
+        this.customAuthenticationProvider = customAuthenticationProvider;
+    }
+
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-
+                .authenticationProvider(customAuthenticationProvider)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/authors/**", "/api/categories/**").hasRole("ADMIN")
                         .requestMatchers("/api/books/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        .requestMatchers("/api/users/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
-                // 3. Use Basic Auth instead of Form Login for REST APIs
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
