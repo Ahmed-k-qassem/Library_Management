31468a4543f88d8cd6acb273ffdf0ddda8dd4014
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-15 15:53:58 +0300

    added the validation for the user requestDTO

--- src/main/java/com/librarymanagment/LibraryManagment/dto/Request/UserRequestDTO.java
+++ src/main/java/com/librarymanagment/LibraryManagment/dto/Request/UserRequestDTO.java
@@ -1,4 +1,13 @@
 package com.librarymanagment.LibraryManagment.dto.Request;
 
-public record UserRequestDTO(String username, String password) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDTO(
+        @NotNull
+        @Size(min = 8)
+        String username,
+        @NotNull
+        @Size(min = 5, max = 20)
+        String password) {
 }
