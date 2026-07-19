91f2edccf2f97db7914e8d257a6d2d47eae0c3bc
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-19 14:49:21 +0300

    category mapper creation

---
+++ src/main/java/com/librarymanagment/LibraryManagment/util/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.librarymanagment.LibraryManagment.util.mapper;
+
+import com.librarymanagment.LibraryManagment.Entities.Category;
+import com.librarymanagment.LibraryManagment.dto.Request.CategoryRequestDTO;
+import com.librarymanagment.LibraryManagment.dto.Response.CategoryResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public CategoryResponseDTO mapCategoryToResponseDTO(Category category){
+        return new CategoryResponseDTO(category.getId(), category.getName());
+    }
+
+
+    public Category mapRequestDTOtoCategory(CategoryRequestDTO categoryRequestDTO){
+        return new Category(categoryRequestDTO.name());
+    }
+}
