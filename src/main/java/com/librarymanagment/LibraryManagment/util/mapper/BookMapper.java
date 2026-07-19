fa8ea4226195eb8b47a895793532347f2f2c3f57
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-19 14:46:50 +0300

    creation of the book mapper

---
+++ src/main/java/com/librarymanagment/LibraryManagment/util/mapper/BookMapper.java
@@ -0,0 +1,36 @@
+package com.librarymanagment.LibraryManagment.util.mapper;
+
+import com.librarymanagment.LibraryManagment.Entities.Author;
+import com.librarymanagment.LibraryManagment.Entities.Book;
+import com.librarymanagment.LibraryManagment.Entities.Category;
+import com.librarymanagment.LibraryManagment.dto.Request.BookRequestDTO;
+import com.librarymanagment.LibraryManagment.dto.Response.BookResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookMapper {
+    public BookResponseDTO mapBookToResponseDTO(Book book) {
+        return new BookResponseDTO(
+                book.getId(),
+                book.getTitle(),
+                book.getIsbn(),
+                book.getPageCount(),
+                book.getStatus(),
+                book.getAddedDate(),
+                book.getAuthor().getAuthorName(),
+                book.getCategory().getName()
+        );
+    }
+
+
+    public Book mapRequestDTOtoBook(BookRequestDTO dto, Author author, Category category) {
+        Book newBook = new Book();
+        newBook.setTitle(dto.title());
+        newBook.setIsbn(dto.isbn());
+        newBook.setPageCount(dto.pageCount());
+        newBook.setStatus(dto.status());
+        newBook.setAuthor(author);
+        newBook.setCategory(category);
+        return newBook;
+    }
+}
