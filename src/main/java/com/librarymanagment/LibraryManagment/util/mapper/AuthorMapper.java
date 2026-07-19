0cf225c4290c1f11e19c5d51f81eb8ee4ccc3e60
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-19 14:43:23 +0300

    added an author mapper

---
+++ src/main/java/com/librarymanagment/LibraryManagment/util/mapper/AuthorMapper.java
@@ -0,0 +1,18 @@
+package com.librarymanagment.LibraryManagment.util.mapper;
+
+import com.librarymanagment.LibraryManagment.Entities.Author;
+import com.librarymanagment.LibraryManagment.dto.Request.AuthorRequestDTO;
+import com.librarymanagment.LibraryManagment.dto.Response.AuthorResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorMapper {
+    public AuthorResponseDTO mapAuthorToResponseDTO(Author author) {
+        return new AuthorResponseDTO(author.getId(),author.getAuthorName(), author.getNationality());
+    }
+
+
+    public Author mapRequestDTOToAuthor(AuthorRequestDTO authorRequestDTO) {
+        return new Author(authorRequestDTO.authorName(),authorRequestDTO.nationality());
+    }
+}
