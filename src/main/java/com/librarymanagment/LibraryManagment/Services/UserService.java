b24459556de25749cacc69517d9a67b12f06216a
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-19 14:57:56 +0300

    added user saving logic to return only response dto entity

--- src/main/java/com/librarymanagment/LibraryManagment/Services/UserService.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Services/UserService.java
@@ -4,7 +4,7 @@ import com.librarymanagment.LibraryManagment.Entities.User;
 import com.librarymanagment.LibraryManagment.Repostries.UserRepository;
 import com.librarymanagment.LibraryManagment.dto.Request.UserRequestDTO;
 import com.librarymanagment.LibraryManagment.dto.Response.UserResponseDTO;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.librarymanagment.LibraryManagment.util.mapper.UserMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,19 +16,20 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final UserMapper userMapper;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder,  UserMapper userMapper) {
         this.userRepository = userRepository;
-        this.passwordEncoder = new BCryptPasswordEncoder();
+        this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
     }
 
 
     @Transactional
-    public User save(UserRequestDTO userRequestDTO) {
-        User  user = new User();
-        user.setUsername(userRequestDTO.username());
-        user.setPassword(passwordEncoder.encode(userRequestDTO.password()));
-        user.setRole("USER");
-        return userRepository.save(user);
+    public UserResponseDTO save(UserRequestDTO userRequestDTO) {
+        User user = userMapper.mapUserRequestDTOtoUser(userRequestDTO, "USER");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user = userRepository.save(user);
+        return userMapper.mapUserToResponseDTO(user);
     }
 
 
@@ -37,13 +38,14 @@ public class UserService {
     }
 
 
-    public UserResponseDTO castUserToResponseDTO(User user){
-        return new UserResponseDTO(user.getId(), user.getUsername(), user.getRole());
-    }
 
 
 
-    public List<User> findAll(){
-        return userRepository.findAll();
+
+    public List<UserResponseDTO> findAll(){
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::mapUserToResponseDTO)
+                .toList();
     }
 }
