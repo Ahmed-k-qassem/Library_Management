b78452c0be2e86b97718709c18a8b24bc549292b
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-19 14:47:18 +0300

    changed the name from bookName to title for consistency

--- src/main/java/com/librarymanagment/LibraryManagment/dto/Response/BookResponseDTO.java
+++ src/main/java/com/librarymanagment/LibraryManagment/dto/Response/BookResponseDTO.java
@@ -2,5 +2,5 @@ package com.librarymanagment.LibraryManagment.dto.Response;
 
 import java.util.Date;
 
-public record BookResponseDTO(long id, String bookName, String isbn, int pageCount, String status, Date addedDate, String authorName, String categoryName) {
+public record BookResponseDTO(long id, String title, String isbn, int pageCount, String status, Date addedDate, String authorName, String categoryName) {
 }
