2941c68ff82215b6c728050fd03a64d7f19caa25
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-23 14:32:29 +0300

    request dto for customer implementation

---
+++ src/main/java/com/librarymanagment/LibraryManagment/dto/Request/CustomerRequestDTO.java
@@ -0,0 +1,8 @@
+package com.librarymanagment.LibraryManagment.dto.Request;
+
+import jakarta.validation.constraints.Size;
+
+public record CustomerRequestDTO(
+        @Size(min = 20, message = "The minimum size of the costumer name should be at least 20")
+        String name){
+}
