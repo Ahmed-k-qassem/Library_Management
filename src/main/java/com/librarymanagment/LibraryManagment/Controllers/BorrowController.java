899d865f839172c63431868e491dce28fb97b33a
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-23 14:30:24 +0300

    added simple controller implementation for borrow end point

---
+++ src/main/java/com/librarymanagment/LibraryManagment/Controllers/BorrowController.java
@@ -0,0 +1,31 @@
+package com.librarymanagment.LibraryManagment.Controllers;
+
+import com.librarymanagment.LibraryManagment.Services.BorrowService;
+import com.librarymanagment.LibraryManagment.dto.Request.BorrowRequestDTO;
+import com.librarymanagment.LibraryManagment.dto.Response.BorrowResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/borrow")
+public class BorrowController {
+
+    private final BorrowService borrowService;
+
+    public BorrowController(BorrowService borrowService) {
+        this.borrowService = borrowService;
+    }
+
+    @GetMapping
+    public List<BorrowResponseDTO> getAllBorrowings(){
+        return borrowService.getAllBorrows();
+    }
+
+    @PostMapping
+    public ResponseEntity<BorrowResponseDTO> createBorrow(@RequestBody BorrowRequestDTO borrowRequestDTO){
+        return new ResponseEntity<>(borrowService.borrowBook(borrowRequestDTO), HttpStatus.CREATED);
+    }
+}
