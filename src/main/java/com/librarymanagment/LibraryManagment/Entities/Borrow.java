d116119681686d557671b9bcae7e4c296e269335
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-23 14:30:03 +0300

    added a borrow entity to map the customer and book entity together

---
+++ src/main/java/com/librarymanagment/LibraryManagment/Entities/Borrow.java
@@ -0,0 +1,67 @@
+package com.librarymanagment.LibraryManagment.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Borrow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Book book;
+
+
+    @ManyToOne
+    private Customer customer;
+
+    private LocalDateTime borrowDate;
+
+    public Borrow(){
+        this.borrowDate = null;
+        this.book = null;
+        this.customer = null;
+    }
+
+
+    public Borrow(Book book, Customer customer){
+        this.book = book;
+        this.customer = customer;
+        this.borrowDate = LocalDateTime.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public LocalDateTime getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDateTime borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+}
