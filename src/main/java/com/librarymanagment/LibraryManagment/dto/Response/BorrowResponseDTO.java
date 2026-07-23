05e2a6eccffe5c7c2d27b8907a45da44b7548cbd
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-23 14:31:21 +0300

    response dto implementation

---
+++ src/main/java/com/librarymanagment/LibraryManagment/dto/Response/BorrowResponseDTO.java
@@ -0,0 +1,6 @@
+package com.librarymanagment.LibraryManagment.dto.Response;
+
+import java.time.LocalDateTime;
+
+public record BorrowResponseDTO(long borrowId, String bookTitle, String customerName, LocalDateTime borrowDate){
+}
