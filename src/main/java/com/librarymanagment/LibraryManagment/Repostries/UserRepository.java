37976d034a1aecafbe928d206baf8b57ac1762a2
Ahmed7749 <ahmadkmm2@gmail.com>
2026-07-31 07:14:28 +0300

    added delete by user id method

--- src/main/java/com/librarymanagment/LibraryManagment/Repostries/UserRepository.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Repostries/UserRepository.java
@@ -2,9 +2,18 @@ package com.librarymanagment.LibraryManagment.Repostries;
 
 import com.librarymanagment.LibraryManagment.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id = :id")
+    int deleteUserById(@Param("id") long id);
 }
