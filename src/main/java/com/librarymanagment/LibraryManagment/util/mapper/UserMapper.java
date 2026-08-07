d14da083529d183ef6eb0433b2d440ab907af2f8
Ahmed7749 <ahmadkmm2@gmail.com>
2026-08-07 09:19:23 +0300

    instead of password its set to uuid

--- src/main/java/com/librarymanagment/LibraryManagment/util/mapper/UserMapper.java
+++ src/main/java/com/librarymanagment/LibraryManagment/util/mapper/UserMapper.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
-    public UserResponseDTO mapUserToResponseDTO(User user){
-        return new UserResponseDTO(user.getId(), user.getUsername(), user.getRole());
+
+    public UserResponseDTO mapUserToResponseDTO(User user) {
+        return new UserResponseDTO(user.getId(), user.getUsername(), user.getRole(), user.getKeycloakUserId());
     }
 
-    public User mapUserRequestDTOtoUser(UserRequestDTO dto, String role){
-        return new User(dto.username(),dto.password(), role);
+    public User mapUserRequestDTOtoUser(UserRequestDTO dto, String role, String keycloakUserId) {
+        return new User(dto.username(), keycloakUserId, role);
     }
 }
