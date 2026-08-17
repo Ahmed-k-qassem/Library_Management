cddb6f9c880fa73d282a7e63aeec42b50c8902b3
Ahmed <ahmadkmm2@gmail.com>
2026-08-17 23:35:54 +0300

    added pre conditions and post conditions using assert library

--- src/main/java/com/librarymanagment/LibraryManagment/Services/AuthorService.java
+++ src/main/java/com/librarymanagment/LibraryManagment/Services/AuthorService.java
@@ -9,83 +9,77 @@ import com.librarymanagment.LibraryManagment.util.GenericPatcher;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
 @Service
 public class AuthorService {
 
-
     private final AuthorRepository authorRepository;
     private final AuthorMapper authorMapper;
     private final GenericPatcher patchUtil;
+
     public AuthorService(AuthorRepository authorRepository, AuthorMapper authorMapper, GenericPatcher patchUtil) {
         this.authorRepository = authorRepository;
         this.authorMapper = authorMapper;
         this.patchUtil = patchUtil;
     }
 
-
     @Transactional
     public AuthorResponseDTO saveAuthor(AuthorRequestDTO authorDTO) {
+        Assert.notNull(authorDTO, "Dev error: Author request id cannot be null");
         Author newAuthor = authorMapper.mapRequestDTOToAuthor(authorDTO);
         Author savedAuthor = authorRepository.save(newAuthor);
         return authorMapper.mapAuthorToResponseDTO(savedAuthor);
     }
 
-
     @Transactional
-    public AuthorResponseDTO updateAuthor(long id, AuthorRequestDTO requestDTO) {
-
+    public AuthorResponseDTO updateAuthor(Long id, AuthorRequestDTO requestDTO) {
+        Assert.notNull(requestDTO, "Dev error: The request dto cannot be null");
         Author existingAuthor = findById(id);
-
         existingAuthor.setAuthorName(requestDTO.authorName());
         existingAuthor.setNationality(requestDTO.nationality());
-
-        Author savedAuthor = authorRepository.save(existingAuthor);
-
-        return authorMapper.mapAuthorToResponseDTO(savedAuthor);
+        return authorMapper.mapAuthorToResponseDTO(existingAuthor);
     }
 
-
-    public List<AuthorResponseDTO> findAll(){
+    public List<AuthorResponseDTO> findAll() {
         return authorRepository.findAll()
                 .stream()
                 .map(authorMapper::mapAuthorToResponseDTO)
                 .toList();
     }
 
-    public Author findById(long id) {
-        return authorRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Author not found"));
+    public Author findById(Long id) {
+        Assert.notNull(id, "Dev error: The id cannot be null");
+        Assert.isTrue(id > 0, "Dev error: The id should be positive or above 0");
+        return authorRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Author not found"));
     }
 
-    public AuthorResponseDTO findByIdToResponse(long id){
-        return authorMapper.mapAuthorToResponseDTO(authorRepository.findAuthorById(id).orElseThrow(() -> new EntityNotFoundException("Author with ID " + id + " has not been found ")));
+    public AuthorResponseDTO findByIdToResponse(Long id) {
+        Assert.notNull(id, "Dev error: The id cannot be null");
+        Assert.isTrue(id > 0, "Dev error: The id should be positive or above 0");
+        return authorMapper.mapAuthorToResponseDTO(findById(id));
     }
-
 
     @Transactional
-    public void delete(Long id){
+    public void delete(Long id) {
+        Assert.notNull(id, "Dev error: The id cannot be null");
+        Assert.isTrue(id > 0, "Dev error: Id should be positive or above 0");
         int rowsAffected = authorRepository.deleteAuthorById(id);
-        if(rowsAffected == 0) {
+        if (rowsAffected == 0) {
             throw new EntityNotFoundException("Author not found");
         }
     }
 
-
     @Transactional
-    public AuthorResponseDTO patchAuthor(long id, String patchBody) {
-
+    public AuthorResponseDTO patchAuthor(Long id, String patchBody) {
+        Assert.notNull(patchBody, "Dev error: The patch body sent is null");
         Author existingAuthor = findById(id);
-
         AuthorResponseDTO dtoToPatch = authorMapper.mapAuthorToResponseDTO(existingAuthor);
-
         AuthorResponseDTO patchedDto = patchUtil.applyPatch(patchBody, dtoToPatch, AuthorResponseDTO.class);
-
         existingAuthor.setAuthorName(patchedDto.authorName());
         existingAuthor.setNationality(patchedDto.nationality());
-
-        return authorMapper.mapAuthorToResponseDTO(authorRepository.save(existingAuthor));
+        return authorMapper.mapAuthorToResponseDTO(existingAuthor);
     }
 }
