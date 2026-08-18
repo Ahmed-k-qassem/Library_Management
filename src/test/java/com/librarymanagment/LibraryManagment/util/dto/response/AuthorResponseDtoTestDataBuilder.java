c6892e56f419b88673584da09cb103979e362590
Ahmed <ahmadkmm2@gmail.com>
2026-08-18 20:55:37 +0300

    a test builder class to decrease duplication in testing code

---
+++ src/test/java/com/librarymanagment/LibraryManagment/util/dto/response/AuthorResponseDtoTestDataBuilder.java
@@ -0,0 +1,33 @@
+package com.librarymanagment.LibraryManagment.util.dto.response;
+
+import com.librarymanagment.LibraryManagment.dto.Response.AuthorResponseDTO;
+
+public class AuthorResponseDtoTestDataBuilder {
+    private Long id = 1L;
+    private String name = "default author";
+    private String nationality = "Unknown";
+
+    public static AuthorResponseDtoTestDataBuilder anAuthorResponseDto(){
+        return new AuthorResponseDtoTestDataBuilder();
+    }
+
+    public AuthorResponseDtoTestDataBuilder withId(Long id){
+        this.id = id;
+        return this;
+    }
+
+    public AuthorResponseDtoTestDataBuilder withName(String name){
+        this.name = name;
+        return this;
+    }
+
+
+    public AuthorResponseDtoTestDataBuilder withNationality(String nationality){
+        this.nationality = nationality;
+        return this;
+    }
+
+    public AuthorResponseDTO build(){
+        return new AuthorResponseDTO(id,name,nationality);
+    }
+}
