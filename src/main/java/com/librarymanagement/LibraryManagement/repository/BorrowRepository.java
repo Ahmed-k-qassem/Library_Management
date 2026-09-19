26b37a13786e4748ddf5dfba3e79c71813ebc8d5
Ahmed <ahmadkmm2@gmail.com>
2026-09-19 23:04:58 +0300

    pagination implemnetaion

--- src/main/java/com/librarymanagement/LibraryManagement/repository/BorrowRepository.java
+++ src/main/java/com/librarymanagement/LibraryManagement/repository/BorrowRepository.java
@@ -1,10 +1,23 @@
 package com.librarymanagement.LibraryManagement.repository;
 
+import com.librarymanagement.LibraryManagement.dto.Response.BorrowResponseDTO;
 import com.librarymanagement.LibraryManagement.entity.Borrow;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BorrowRepository extends JpaRepository<Borrow, Long>
-{
+public interface BorrowRepository extends JpaRepository<Borrow, Long> {
+
+    @Query(value = """
+            select new com.librarymanagement.LibraryManagement.dto.Response.BorrowResponseDTO(
+                br.id, b.title, c.name, br.borrowDate)
+            from Borrow br
+            left join br.book b
+            left join br.customer c
+            """,
+            countQuery = "select count(br) from Borrow br")
+    Page<BorrowResponseDTO> findAllSummaries(Pageable pageable);
 }
