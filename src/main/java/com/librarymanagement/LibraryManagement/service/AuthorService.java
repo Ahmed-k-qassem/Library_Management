c3868ad7f6d9ce3787ee9503418ea1c490b69588
Ahmed <ahmadkmm2@gmail.com>
2026-09-19 23:00:00 +0300

    modified the findAll method to return the page

--- src/main/java/com/librarymanagement/LibraryManagement/service/AuthorService.java
+++ src/main/java/com/librarymanagement/LibraryManagement/service/AuthorService.java
@@ -7,11 +7,11 @@ import com.librarymanagement.LibraryManagement.dto.Response.AuthorResponseDTO;
 import com.librarymanagement.LibraryManagement.util.mapper.AuthorMapper;
 import com.librarymanagement.LibraryManagement.util.GenericPatcher;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-
-import java.util.List;
 
 @Service
 public class AuthorService {
@@ -43,11 +43,8 @@ public class AuthorService {
         return authorMapper.mapAuthorToResponseDTO(authorRepository.save(existingAuthor));
     }
 
-    public List<AuthorResponseDTO> findAll() {
-        return authorRepository.findAll()
-                .stream()
-                .map(authorMapper::mapAuthorToResponseDTO)
-                .toList();
+    public Page<AuthorResponseDTO> findAll(Pageable pageable) {
+        return authorRepository.findAllSummaries(pageable);
     }
 
     public Author findById(Long id) {
