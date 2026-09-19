3b480e6593a8d06a45e460abf728e7205a69dec9
Ahmed <ahmadkmm2@gmail.com>
2026-09-19 23:05:27 +0300

    handled the invlaid sort exception

---
+++ src/main/java/com/librarymanagement/LibraryManagement/exception/InvalidSortException.java
@@ -0,0 +1,19 @@
+package com.librarymanagement.LibraryManagement.exception;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class InvalidSortException extends RuntimeException {
+
+    private final Set<String> allowedProperties;
+
+    public InvalidSortException(Set<String> allowedProperties) {
+        super("Invalid sort property. Allowed values: "
+                + String.join(", ", new TreeSet<>(allowedProperties)));
+        this.allowedProperties = Set.copyOf(allowedProperties);
+    }
+
+    public Set<String> getAllowedProperties() {
+        return allowedProperties;
+    }
+}
